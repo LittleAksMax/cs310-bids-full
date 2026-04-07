560099ba7f9c346884925ed699fcda81806ea642
David R <david2005.rosental@gmail.com>
2026-04-07 22:48:33 +0100

    Added platform dependence to docker image building (development machine does not have the same architecture as deployment server)

--- push-to-ecr.sh
+++ push-to-ecr.sh
@@ -2,12 +2,13 @@
 AWSACCID="$1"
 IMAGE="$2"
 VERSION="$3"
+PLATFORM="${4:-linux/arm64}" # we default to ARM because dev machine is Apple Silicon
 
-echo "Acc. ID: $AWSACCID -- Image: $IMAGE -- Vers.: $VERSION"
+echo "Acc. ID: $AWSACCID -- Image: $IMAGE -- Vers.: $VERSION -- Platform: $PLATFORM"
 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $AWSACCID.dkr.ecr.eu-west-2.amazonaws.com
 
-docker build -t bidder/$IMAGE:$VERSION .
+docker build --platform $PLATFORM -t bidder/$IMAGE:$VERSION .
 
 docker tag bidder/$IMAGE:$VERSION $AWSACCID.dkr.ecr.eu-west-2.amazonaws.com/bidder/$IMAGE:$VERSION
 
